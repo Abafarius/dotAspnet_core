602f306a81ef6238720ad25763f7d4b3740b51c9
Yessimbek Galymzhan <esm.2001@mail.ru>
2024-12-18 21:32:10 +0500

    Update Program.cs

--- FirstApp/FirstApp/Program.cs
+++ FirstApp/FirstApp/Program.cs
@@ -1,5 +1,6 @@
 using FirstApp;
 using FirstApp.Model;
+using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSwaggerGen();
@@ -9,9 +10,12 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 //var data = new Data();
+ICommand commandFruit = new CommandFruit();
+
+
 app.MapGet("/fruit/all", () => Data.all);
 app.MapGet("/fruit/{id}", (int id) => Data.all[id]);
-app.MapPost("/friut/{id}", Command.CreateFruit);
-app.MapPut("/fruit/{id}", Command.UpdateFruit);
-app.MapDelete("/fruit/{id}", Command.DeleteFruit);
+app.MapPost("/friut/{id}", commandFruit.CreateFruit).WithParameterValidation();
+app.MapPut("/fruit/{id}", commandFruit.UpdateFruit);
+app.MapDelete("/fruit/{id}", commandFruit.DeleteFruit);
 app.Run();
