38caf4eb7e6e99902a4042c82b7e72b158f126ee
Yessimbek Galymzhan <esm.2001@mail.ru>
2024-12-18 19:55:19 +0500

    Create task.cs

---
+++ FirstApp/FirstApp/Model/task.cs
@@ -0,0 +1,18 @@
+﻿namespace FirstApp.Model
+{
+    public class TaskManager
+    {
+        public string? TaskName { get; set; } // Название задачи
+        public string? Description { get; set; } // Описание задачи
+        public bool IsCompleted { get; set; } // Статус выполнения задачи
+
+        // Конструктор для инициализации задачи
+        public TaskManager(string? taskName, string? description, bool isCompleted)
+        {
+            TaskName = taskName;
+            Description = description;
+            IsCompleted = isCompleted;
+        }
+    }
+
+}
