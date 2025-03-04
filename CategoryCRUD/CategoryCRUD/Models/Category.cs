92d7b911926b23a54da1ada7bcb69e70aaaada2c
Yessimbek Galymzhan <esm.2001@mail.ru>
2025-03-04 20:32:23 +0500

    Create Category.cs

---
+++ CategoryCRUD/CategoryCRUD/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace CategoryCRUD.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public required string Name { get; set; }
+
+
+        [Range(1, 100)]
+        public int DisplayOrder { get; set; }
+    }
+}
