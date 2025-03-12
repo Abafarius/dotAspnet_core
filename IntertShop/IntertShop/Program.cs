a9d03ed2cace6b3d8208ab285bffbf2b1e7c66b4
gal.esm49 <esm.2001@mail.ru>
2025-03-12 19:31:30 +0500

    Update Program.cs

--- IntertShop/IntertShop/Program.cs
+++ IntertShop/IntertShop/Program.cs
@@ -1,3 +1,4 @@
+using InternetShop.DataAccess.Repository.Categories;
 using IntertShop.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
